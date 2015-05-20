8d37828d5b54021879541ea5111364ea41d9168b
jfm <jfm@jfm-Satellite-L635>
2015-05-20 09:24:55 -0500

    Iteracion 5 - App corriendo en JBoss intento 1

--- backend/src/main/java/serviciosEnsayo/Ensayo.java
+++ backend/src/main/java/serviciosEnsayo/Ensayo.java
@@ -5,7 +5,7 @@
  */
 package serviciosEnsayo;
 
-import java.sql.Connection;
+import javax.jms.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -21,7 +21,14 @@ import javax.ws.rs.Path;
 
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 /**
  *
  * @author Jonathan
@@ -30,13 +37,54 @@ import javax.ws.rs.core.MediaType;
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class Ensayo {
+    
+    public ConnectionFactory cf;
+    public Connection c;
+    public Session s;
+    public Destination d;
+    public MessageProducer mp;
+    /*
+    private final static String JNDI_FACTORY ="org.jboss.naming.remote.client.InitialContextFactory";
+    private final static String JMS_FACTORY = "jms/RemoteConnectionFactory";
+    private final static String QUEUE = "jms/queue/test";
+    private final static String jbossUrl = "remote://localhost:4447";
+ 
+    private static InitialContext getInitialContext() throws NamingException {
+    Hashtable env = new Hashtable();
+    env.put(Context.INITIAL_CONTEXT_FACTORY, JNDI_FACTORY);
+    env.put(Context.PROVIDER_URL, jbossUrl);
+    env.put(Context.SECURITY_PRINCIPAL, "jms");
+    env.put(Context.SECURITY_CREDENTIALS, "jboss1");
+    return new InitialContext(env);
+    } */
+    
+    public void iniciar() throws NamingException, JMSException {
+        InitialContext init = new InitialContext();
+        cf = (ConnectionFactory) init.lookup("jms/RemoteConnectionFactory");
+        d = (Destination) init.lookup("jms/queue/PlayQueue");
+        c = (Connection) cf.createConnection("sistrans", "iteracion");
+        c.start();
+        s = c.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        mp = s.createProducer(d);
+    }
+    
+     private void send(String string) throws JMSException {
+        TextMessage tm = s.createTextMessage(string);
+        mp.send(tm);
+        }
 
+        private void close() throws JMSException {
+        this.c.close();
+        }
+    
     @GET
     public Integer getTodosLosMuebles() {
         return 1;
 
     }
 
+    
+    
 //    @POST
 //    @Path("/metodo")
 //    public String metodo(List lista) {
@@ -71,27 +119,15 @@ public class Ensayo {
     public String metodo3() {
 
         try {
-            Connection con = null;
-            Class.forName("oracle.jdbc.driver.OracleDriver");
-            con = DriverManager.getConnection("jdbc:oracle:thin:@157.253.238.224:1531:prod", "ISIS2304271510", "rproxyquark");
-            
-            String sql = "select * from PRUEBA";
-                //System.out.println(sql);
-            Statement st = con.createStatement();
-            
-            ResultSet rs = st.executeQuery(sql);
-            String resps = "";
-            while (rs.next()){   
-                
-                resps+=rs.getString("COLUMN1")+" , "+rs.getString("COLUMN2")+"\n";
-            }
-            
-            return resps;
+             iniciar();
+             send("Ola ca estou eu!");
+             s.close();
+             return "Bien";
             
         } catch (Exception e) {
             e.printStackTrace();
+            return "Mal";
         }
         
-        return "Error";
     }
 }
